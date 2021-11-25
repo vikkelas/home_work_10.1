4247084466d225e912ca7be8f64d789f620124a5
vikkelas <bass89@mail.ru>
2021-11-25 19:39:22 +0300

    final

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -1,18 +1,9 @@
 /* eslint-disable linebreak-style */
 import json from './Json';
 import read from './Read';
-import SaveGaming from './GameSaving';
 
 export default class GameSavingLoader {
   static load() {
-    return read().then((value) => json(value)).then((value2) => {
-      const valueJson = JSON.parse(value2);
-      const saveObj = new SaveGaming(valueJson.id, valueJson.created, valueJson.userInfo);
-      return saveObj;
-    }).catch((err) => {
-      err.toString();
-    });
+    return read().then((value) => json(value));
   }
 }
-// не понимаю правда зачем нужен catch
-// если в промиссах нет reject
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
+/* eslint-disable linebreak-style */
+import GameSavingLoader from './GameSavingLoader';
+import GameSaving from './GameSaving';
 
+GameSavingLoader.load().then((value) => {
+  const obj = JSON.parse(value);
+  const save = new GameSaving(obj.id, obj.created, obj.userInfo);
+  return save;
+});
